6e33998b2251f374df8724d8b85ffe811e6d402c
Jesse Hu <huyouare@gmail.com>
2014-06-03 16:34:19 -0700

    Moved code to freebase-try, added scraper.py

--- scraper.py
+++ scraper.py
@@ -1,23 +1,5 @@
+from bs4 import BeautifulSoup
+import csv
 import json
-import urllib
 
-api_key = open(".freebase_api_key").read()
-service_url = 'https://www.googleapis.com/freebase/v1/mqlread'
-query = [{
-  "type": "/type/namespace",
-  "id": "/authority/nces/ipeds",
-  "key": [{}],
-  "keys": [{}]
-}]
-params = {
-        'query': json.dumps(query),
-        'key': api_key
-}
-url = service_url + '?query=' + params['query']  + '&cursor'
-print url
-response = json.loads(urllib.urlopen(url).read())
-result = response['result']
-for key in result[0]['keys']:
-  print key
-print len(result[0]['keys'])
-print response
+soup = BeautifulSoup(html_doc)
