0f730112e771937c38f73ca6b3d5362225b21921
Jesse Hu <huyouare@gmail.com>
2014-06-10 14:02:01 -0700

    Changed data-diff-accred to use names and OPE ID

--- data-diff-accred.py
+++ data-diff-accred.py
@@ -11,11 +11,13 @@ with open('./datasets/ipeds_data_for_production_database.csv', 'rU') as csvfile:
 
 # vs accred
 accred_colleges = set()
+accred_rows = dict()
 
 with open('accredidation-ipeds.csv', 'rU') as csvfile:
   reader = csv.reader(csvfile)
   for row in reader:
     accred_colleges.add(row[8])
+    accred_rows[row[8]] = row
 
 print len(ipeds_colleges)
 print len(accred_colleges)
@@ -28,10 +30,13 @@ diff.sort()
 for x in diff:
   outfile.write( x + '\n' )
 
-outfile = open('./diff-output/accred-vs-ipeds.txt', 'w')
+outfile = open('./diff-output/accred-vs-ipeds-with-names.txt', 'w')
+writer = csv.writer(outfile)
 
 diff = accred_colleges - ipeds_colleges
 diff = list(diff)
 diff.sort()
+writer.writerow(['IPEDS ID', 'School Name', 'OPE ID'])
 for x in diff:
-  outfile.write( x + '\n' )
+  row = accred_rows[x]
+  writer.writerow( [x, row[1], row[7]] )
